16e6df4ec8ee5b068330e4d87943b5a823a43a2c
Kibetkipsang <denniskipsang2000@gmail.com>
2025-11-21 09:30:46 +0300

    fetch with react query implimented

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,10 @@
-import { useEffect, useState } from 'react'
+
+import React, { useState } from 'react'
 import { Button } from './components/ui/button';
 import { Input } from "@/components/ui/input"
+import useDictionaryQuery from './UseDictionary';
+import { Spinner } from './components/ui/spinner';
+
 
 interface Word{
   word: string
@@ -8,11 +12,18 @@ interface Word{
 
 function App() {
   const [word, setWord] = useState("")
+ 
+  const {data, isLoading, isError, refetch} = useDictionaryQuery(word);
+
   function handleChange(e: React.ChangeEvent<HTMLInputElement>){
     setWord(e.target.value)
   }
 
-  useEffect()
+  function handleSearch(e: React.FormEvent){
+    e.preventDefault();
+    refetch();
+  }
+
   return (
     <>
       <div className='bg-green-500 text-white min-h-screen p-3'>
@@ -21,7 +32,7 @@ function App() {
           <p className='text-2xl '>One stop word definer!</p>
         </div>
         <div className='w-1/2 mx-auto '>
-          <form className='flex flex-row gap-5'>
+          <form className='flex flex-row gap-5' onSubmit={handleSearch}>
             <Input
             type='text'
             placeholder='type a word you want to search..'
@@ -29,9 +40,38 @@ function App() {
             value={word}
             className='bg-gray-100 text-gray-900'
             />
-            <Button className='bg-orange-500 hover:bg-orange-700'>Search</Button>
+            <Button className='bg-orange-500 hover:bg-orange-700' type='submit' disabled={isLoading}>
+              <div className='flex align-center gap-2 justify-center'>
+                {isLoading && <Spinner/>}
+                {isLoading ? <p>Searching...</p> : <p>Search</p>}
+              </div>
+            </Button>
           </form>
-          
+         <div>
+           <div className='m-4 p-2  font-bold '>
+            {isLoading && <p>Loading Please Wait...</p>}
+            {isError && <p className='m-4 p-2 text-red-500 font-bold bg-white rounded-md'>Cannot find results of your search. Please try finding another word.</p>} 
+           </div>
+          <div>
+            {data && (
+            <div className="mt-5 bg-blue-700 text-white p-4 rounded">
+              <h3 className="text-2xl font-bold">{data[0].word.charAt(0).toUpperCase() + data[0].word.slice(1)}</h3>
+              <p>Pronounciation: {data[0].phonetic}</p>
+
+              <div className='bg-orange-500 rounded p-4 m-2'>
+                <p className="mt-2 text-white">
+               <span className='font-bold'>Definition:</span> {data[0].meanings[0].definitions[0].definition}
+              </p>
+              <div className='bg-orange-600 p-2 rounded m-2'>
+                <p className="mt-2 text-white">
+               <span className='font-bold'>Example:</span> {data[0].meanings[0].definitions[0].example || `Example for the word ( ${word} ) is not provided.`}
+              </p>
+              </div>
+              </div>
+            </div>
+          )}
+          </div>
+         </div>
         </div>
       </div>
     </>
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
+import { Toaster } from "@/components/ui/sonner"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <Toaster />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </StrictMode>
 )
